edf13b9e3a76b56a2c46a0c6f600f6262fb81149
Alexander Karlsson <alexstim@hotmail.no>
2020-03-27 01:12:24 +0100

    yup

--- assignments/assignment3/src/main.py
+++ assignments/assignment3/src/main.py
@@ -13,8 +13,6 @@ def main():
 
     screen_size = [P.SCREEN_WIDTH, P.SCREEN_HEIGHT]
     screen = pygame.display.set_mode(screen_size)                   
-    background_image = pygame.image.load("pics/bg.png")
-    background_image = pygame.transform.scale(background_image, (P.SCREEN_WIDTH, P.SCREEN_HEIGHT))
 
     pygame.display.set_caption("Mayhem: World leader edition")
     font = pygame.font.Font('freesansbold.ttf', 32) 
@@ -22,14 +20,10 @@ def main():
     running = True 
     time = pygame.time.Clock()
 
-    # Tests
-
+    background = Screen_Obj("pics/bg.png", P.SCREEN_WIDTH, P.SCREEN_HEIGHT, (800, 450))
     millennium_falcon = Millennium_Falcon()
     star_destroyer = Star_Destroyer()
-    player_list = pygame.sprite.Group(millennium_falcon, star_destroyer)
-
-
-    # Test end 
+    player_list = pygame.sprite.Group(background, millennium_falcon, star_destroyer)
 
     while running:
         for event in pygame.event.get():
@@ -41,9 +35,8 @@ def main():
         if key[pygame.K_ESCAPE]:
             running = False
 
-        screen.blit(background_image, (0, 0))
-
         fps = time.tick(60)
+        
         player_list.draw(screen)                                        # Blit bilde, test
 
         star_destroyer.update(fps)
