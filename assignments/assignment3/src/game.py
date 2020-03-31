c586480a99059c1f07d29b896ac7b6c35e598fd3
Alexander Karlsson <alexstim@hotmail.no>
2020-03-31 13:32:51 +0200

    wtf

--- assignments/assignment3/src/game.py
+++ assignments/assignment3/src/game.py
@@ -21,11 +21,8 @@ class Game(pygame.sprite.Sprite):
         pygame.display.set_icon(self.icon)
         self.time       = pygame.time.Clock()                           # Gets time, used for update frequenzy
         self.fps        = self.time.tick(60)                            # Set update frequency(fps) to 60
-        self.text       = self.font.render("Press SPACE to restart", False, (0, 0, 0))
 
         self.background = Screen_Obj("pics/bg.png", C.SCREEN_WIDTH, 
-        C.SCREEN_HEIGHT, (C.SCREEN_WIDTH/2, C.SCREEN_HEIGHT/2))
-        self.restart    = Screen_Obj("pics/bg.png", C.SCREEN_WIDTH, 
         C.SCREEN_HEIGHT, (C.SCREEN_WIDTH/2, C.SCREEN_HEIGHT/2))
         self.restart_t  = Screen_Obj("pics/restart.png", 600, 100, (C.SCREEN_WIDTH/2, C.SCREEN_HEIGHT/2))
         self.player1    = Millennium_Falcon()
@@ -62,15 +59,13 @@ class Game(pygame.sprite.Sprite):
             
             if self.player2 not in self.group or self.player1 not in self.group:
                 self.group.empty()
-                self.group.add(self.restart, self.restart_t)
+                self.group.add(self.background, self.restart_t)
                 if key[pygame.K_SPACE]:
                     self.group.empty()
                     self.player1 = Millennium_Falcon()
                     self.player2 = Star_Destroyer()
                     self.group.add(self.background, self.player1, self.player2)
                 
-
-
             pygame.display.flip()
     
         pygame.quit()
@@ -79,17 +74,18 @@ class Game(pygame.sprite.Sprite):
 
         for blast in self.player1.blast_list:
             if pygame.sprite.collide_rect(blast, self.player2):
-            #if collision == True:
                 boom = Explosion(self.player2.rect.center)
                 boom.update()
                 self.group.add(boom)
                 self.group.remove(self.player2)
+        
+        if pygame.sprite.collide_rect(self.player1, self.player2):
+            self.group.remove(self.player1, self.player2)
 
     def player2_collision(self):
 
         for blast in self.player2.blast_list:
             if pygame.sprite.collide_rect(blast, self.player1):
-            #if collision == True:
                 boom = Explosion(self.player1.rect.center)
                 boom.update()
                 self.group.add(boom)
